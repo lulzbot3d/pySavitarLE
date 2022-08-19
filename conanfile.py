86e5e4ac988ae5f2161cc24e0a54f3f7d19d819b
Joey de l'Arago <joeydelarago@gmail.com>
2022-08-19 12:54:28 +0200

    Update minimum conan version

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from conan import ConanFile
 from conan.tools.layout import basic_layout
 from conans import tools
 
-required_conan_version = ">=1.46.2"
+required_conan_version = ">=1.48.0"
 
 class PySavitarConan(ConanFile):
     name = "pysavitar"
