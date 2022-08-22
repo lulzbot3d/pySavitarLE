6e18755d815f48883389a6714a1e47e34bd79834
j.spijker@ultimaker.com <jelle spijker>
2022-08-22 11:45:40 +0200

    Use CMake build modules from umbase and sipbuildtool
    COntributes to CURA-8788

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
 from conan.tools import files
 from conan import ConanFile
-from conan.tools.layout import basic_layout
 from conans import tools
 
 required_conan_version = ">=1.48.0"
@@ -21,7 +20,7 @@ class PySavitarConan(ConanFile):
     revision_mode = "scm"
     exports = "LICENSE*"
 
-    python_requires = "umbase/0.1.5@ultimaker/testing", "pyprojecttoolchain/0.1.3@ultimaker/testing", "sipbuildtool/0.2.0@ultimaker/testing"
+    python_requires = "umbase/0.1.6@ultimaker/stable", "pyprojecttoolchain/0.1.5@ultimaker/stable", "sipbuildtool/0.2.2@ultimaker/stable"
     python_requires_extend = "umbase.UMBaseConanfile"
 
     options = {
@@ -44,8 +43,10 @@ class PySavitarConan(ConanFile):
     }
 
     def requirements(self):
+        self.requires("umbase/0.1.6@ultimaker/stable")  # required for the CMake build modules
+        self.requires("sipbuildtool/0.2.2@ultimaker/stable")  # required for the CMake build modules
         for req in self._um_data()["requirements"]:
-            self.requires(req)
+                self.requires(req)
 
     def config_options(self):
         if self.options.shared and self.settings.compiler == "Visual Studio":
@@ -60,6 +61,9 @@ class PySavitarConan(ConanFile):
             tools.check_min_cppstd(self, 17)
 
     def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
         pp = self.python_requires["pyprojecttoolchain"].module.PyProjectToolchain(self)
         pp.blocks["tool_sip_project"].values["sip_files_dir"] = Path("python").as_posix()
         pp.blocks["tool_sip_bindings"].values["name"] = "pySavitar"
@@ -67,26 +71,46 @@ class PySavitarConan(ConanFile):
         pp.blocks.remove("extra_sources")
         pp.generate()
 
+        tc = CMakeToolchain(self)
+        tc.variables["Python_EXECUTABLE"] = self.deps_user_info["cpython"].python.replace("\\", "/")
+        tc.variables["Python_USE_STATIC_LIBS"] = not self.options["cpython"].shared
+        tc.variables["Python_ROOT_DIR"] = self.deps_cpp_info["cpython"].rootpath.replace("\\", "/")
+        tc.variables["Python_FIND_FRAMEWORK"] = "NEVER"
+        tc.variables["Python_FIND_REGISTRY"] = "NEVER"
+        tc.variables["Python_FIND_IMPLEMENTATIONS"] = "CPython"
+        tc.variables["Python_FIND_STRATEGY"] = "LOCATION"
+        tc.variables["Python_SITEARCH"] = "site-packages"
+
+        if self.settings.compiler == "Visual Studio":
+            tc.blocks["generic_system"].values["generator_platform"] = None
+            tc.blocks["generic_system"].values["toolset"] = None
+        tc.generate()
+
+        # Generate the Source code from SIP
+        sip = self.python_requires["sipbuildtool"].module.SipBuildTool(self)
+        sip.configure()
+        sip.build()
+
     def layout(self):
-        basic_layout(self)
+        cmake_layout(self)
 
         if self.settings.os in ["Linux", "FreeBSD", "Macos"]:
             self.cpp.package.system_libs = ["pthread"]
 
     def build(self):
-        sip = self.python_requires["sipbuildtool"].module.SipBuildTool(self)
-        sip.configure()
-        sip.build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
         packager = files.AutoPackager(self)
-        packager.patterns.build.lib = ["*.so", "*.so.*", "*.a", "*.lib", "*.dylib", "*.pyd", "*.pyi"]
+        packager.patterns.build.lib = ["*.so", "*.so.*", "*.a", "*.lib", "*.dylib", "*.pyd"]
         packager.run()
 
-        files.files.rmdir(self, os.path.join(self.package_folder, "lib", "pySavitar"))
+        self.copy("*.pyi", src = os.path.join(self.build_folder, "pySavitar"), dst = os.path.join(self.package_folder, "lib"), keep_path = False)
 
     def package_info(self):
         if self.in_local_cache:
-            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "site-packages"))
+            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "lib"))
         else:
-            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.build_folder, "site-packages"))
+            self.runenv_info.append_path("PYTHONPATH", self.build_folder)
