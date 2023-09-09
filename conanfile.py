2527fe9a8d353cf599bf8102c4a60cbc7b3a0d1c
jellespijker <jelle.spijker@ultimaker.com>
2023-09-09 17:10:24 +0200

    pin to libSavitar 5.3.0
    Contributes to CURA-10591 and CURA-10475

--- conanfile.py
+++ conanfile.py
@@ -45,7 +45,7 @@ class PySavitarConan(ConanFile):
 
     def set_version(self):
         if not self.version:
-            self.version = "5.3.0-alpha"
+            self.version = "5.4.0-alpha"
 
     @property
     def _min_cppstd(self):
@@ -66,7 +66,7 @@ class PySavitarConan(ConanFile):
         copy(self, "*", path.join(self.recipe_folder, "python"), path.join(self.export_sources_folder, "python"))
 
     def requirements(self):
-        self.requires("savitar/(latest)@ultimaker/testing")
+        self.requires("savitar/5.3.0")
         self.requires("cpython/3.10.4")
 
     def validate(self):
